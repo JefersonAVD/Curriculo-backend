d50b435c61daf20a3f3fdba6998d34a1ba3e82b6
Jeferson Dias <jefersondias6711@gmail.com>
2022-11-15 21:20:14 -0300

    testando quemSou.blade

--- routes/web.php
+++ routes/web.php
@@ -56,7 +56,7 @@ Route::middleware(CrudBlock::class)->group( function (){
         Route::get('/{linha}/textos','textoIndex')->name('texto.index');
         Route::post('/{linha}/salvar','textoStore')->name('texto.store');
         Route::delete('/{linha}/deletar/{id}','textoDestroy')->name('texto.destroy');
-    
+        
         Route::get('/','index')->name('quemsou.index');
         Route::get('/{titulo}','edit')->name('quemsou.edit');
         Route::post('/salvar','store')->name('quemsou.store');
