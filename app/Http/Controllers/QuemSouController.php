d3a3baf75c4c2334fb006b6422b5f1c6f5d9f2f9
Jeferson Dias <jefersondias6711@gmail.com>
2022-11-15 21:32:11 -0300

    Atualizando url de quemSou

--- app/Http/Controllers/QuemSouController.php
+++ app/Http/Controllers/QuemSouController.php
@@ -10,7 +10,7 @@ class QuemSouController extends Controller
 {
     public function index(QuemSou $info)
     {
-        return view('quemsou.index',['info'=>$info->all()])->with(['mensagemSucesso'=>session('mensagem.sucesso'),'pageTitle'=>'Quem Sou']);
+        return view('quemSou.index',['info'=>$info->all()])->with(['mensagemSucesso'=>session('mensagem.sucesso'),'pageTitle'=>'Quem Sou']);
     }
     public function store(Request $request)
     {
@@ -36,7 +36,7 @@ class QuemSouController extends Controller
     public function textoIndex(Request $request)
     {
         $linha = QuemSou::with('texto')->find($request->linha);
-        return view('quemSou.Textos.index',['linha'=>$linha])->with('pageTitle',"Textos de '$linha->titulo'");
+        return view('quemSou.textos.index',['linha'=>$linha])->with('pageTitle',"Textos de '$linha->titulo'");
     }
 
     public function textoStore(Request $request)
